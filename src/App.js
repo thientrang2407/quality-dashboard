1b16326f662842037db0d8012b852db057500f24
thientrang2407 <d.thientrang2407@gmail.com>
2021-05-23 17:55:03 +0700

    Create coming soon page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'roboto'
+    }}>
+      <h1 style={{
+        color: '#6EC177',
+        margin: 0,
+      }}>Wah Gwan, Soon Come</h1>
+      <p>What's up, Coming Soon</p>
     </div>
   );
 }
